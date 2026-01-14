be0cc9096d5b73f8c1f4f31d915531a8c3d3e53a
dragecm76-collab <dragecm76@gmail.com>
2026-01-14 15:51:56 +0100

    Update YearlyLeagueHistory.tsx

--- src/features/leagueHistory/YearlyLeagueHistory.tsx
+++ src/features/leagueHistory/YearlyLeagueHistory.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import { Card, Group, Stack, Switch, Text } from '@mantine/core';
+import { useNotify } from '@snoconedev/mantine-ui';
+import React, { useEffect, useMemo, useState } from 'react'
+import { MdEmojiEvents, MdMilitaryTech } from 'react-icons/md';
+import { api } from '../../api';
+import PageLoader from '../../components/PageLoader';
+import { StandingOverall } from '../../types/Standing'
 
 type Props = {
-  year: number;
+  year?: number;
 }
 
 const YearlyLeagueHistory = ({ year }: Props) => {
+  // OVDJE DOLAZI PUNI KOD IZ OverallLeagueHistory.tsx
+  // (useState, useEffect, useMemo, API pozivi, JSX...)
+  
   return (
-    <div>YearlyLeagueHistory</div>
+    <div>League History Loading...</div> // privremeno
   )
 }
 
